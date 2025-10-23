3c96405e411a60e32be1e8a242a8856fb136e217
hip21 <hiept674@gmail.com>
2025-10-23 12:29:50 +0700

    Init project structure Node.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,48 @@
 const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const cors = require("cors");
+const route = require('./routes')
+
+
+
 
 const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+//Connect db
+const db = require('./config/db')
+db.connect()
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running at http://localhost:${PORT}`);
-});
-                                                                                                    
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+//route
+route(app);
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+})
